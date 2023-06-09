0320564f93652593894e4fddc0149ae954a2585a
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-06-09 18:12:12 +0600

    get the Instructor added all class on database

--- src/Pages/Dashboards/InstructorDashboard/InstructorClasses/InstructorClasses.jsx
+++ src/Pages/Dashboards/InstructorDashboard/InstructorClasses/InstructorClasses.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../../Hooks/useAuth';
+import ClassDetails from './ClassDetails';
 
 const InstructorClasses = () => {
+    const [classes,setClasses]=useState([])
+    const {user}=useAuth()
+    useEffect(()=>{
+        fetch(`http://localhost:5000/classes?email=${user.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            setClasses(data)
+        })
+    },[])
+    console.log(classes)
     return (
         <div>
-            <h1>instructorclasses</h1>
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Class Name</th>
+        <th>Instructor</th>
+        <th>Class Price</th>
+        <th>Available seats</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+      {
+        classes.map((singleClass,index)=><ClassDetails key={singleClass._id} singleClass={singleClass} index={index}></ClassDetails>)
+      }
+    </tbody>
+    
+    
+  </table>
+</div>
         </div>
     );
 };
